dbfe82596a80a5591073eabd40b8db0fdbd64c1f
Hakxel <asiel_montes@yahoo.com>
2018-12-09 20:10:43 -0500

    Finish initial version with minor bugs

--- index.js
+++ index.js
@@ -1,72 +1,244 @@
 let readlineSync = require('readline-sync');
-
-
-
-console.log(`\n WELCOME TO BULLS AND COWS`);
-console.log(" ");
-let optionNumb = readlineSync.question(` What do you want to do?\n (Insert number)\n\n  - 1: Play\n  - 2: See instructions\n  - 3: Exit \n`);
-
-let numbers= [];
-let randomNum = 0;
-let bulls = 0;
-let cows = 0;
-let arrComp = [9, 3, 7, 5];
-let arrUser = [7, 3, 9, 2];
+let bulls;
+let cows;
+let randomNum;
+let winNumbers = [];
+let arrComp = [];
+let usrGuess = [];
+let arrsUser = [];
+let guesses = [];
+let attempts = 1
+let winner = false;
 
 function generateNums() {
   for(i = 0; i < 4; i++){
     randomNum = Math.random() * 10;
-    number.push(Math.floor(randomNum));
+    winNumbers.push(Math.floor(randomNum));    
   };
 };
 
-function findBulls(arr1, arr2) {
-  bulls = 0
-  for (i = 0; i < 4; i++) {
-    if (arr1[i] == arr2[i]) {
-      arr1.splice(i, 1, `/`);
-      arr2.splice(i, 1, `/`);
+function playAgain() {
+  console.log(" ")
+  let playAgain = readlineSync.question(` Would you like to play again? (y/n)\n`);
+  playAgain.toLowerCase().trim();
+  if (playAgain == 'y') {
+    play(arrComp);
+  }
+  else if (playAgain == 'n') {
+    exit()
+  }
+  else {
+    console.log(` Please answer 'y' or 'n'\n`)
+  }
+};
+
+
+function findBullsAndCows(comp, usr, win){
+  
+  function findBulls(arr1, arr2) {
+    bulls = 0
+    for (i = 0; i < 4; i++) {
+      if (arr1[i] == arr2[i]) {
+        arr1.splice(i, 1, `B`);
+        arr2.splice(i, 1, `B`);
+      };
     };
   };
-};
-
-
-function findCows(arr1, arr2) {
-  cows = 0
-  for (i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== `/` && arr1[i] !== `*`) {
-      for (j = 0; j < arr2.length; j++) {
-        if (arr1[i] == arr2[j]) {
-          arr1.splice(i, 1, `*`);
-          arr2.splice(j, 1, `*`);
+  
+  
+  function findCows(arr1, arr2) {
+    cows = 0
+    for (i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== `B` && arr1[i] !== `C`) {
+        for (j = 0; j < arr2.length; j++) {
+          if (arr1[i] == arr2[j]) {
+            arr1.splice(i, 1, `C`);
+            arr2.splice(j, 1, `C`);
+          }
         }
       }
     }
   }
+  
+  function finalCount(arr2) {
+    console.log(`procCurUsG: ` + arr2)
+    for (i = 0; i < arr2.length; i++) {
+      if (arr2[i] == `B`) {
+        bulls++;
+      }
+      else if (arr2[i] == `C`) {
+        cows++;
+      }
+      else {
+        continue;
+      }
+    }
+  }
+  
+  function checkWinner(usr, win) {
+    console.log(usr)
+    if (usr.toString().replace(/,/g, '') == 'BBBB') {
+      win = win.toString().replace(/,/g, '');
+      console.log(" ")
+      console.log(` The computer's number is ${win} !!!\n`)
+      console.log(` YOU ARE A WINNER!!!\n`);
+      winner = true;
+    }
+  };
+
+  findBulls(comp, usr);
+  findCows(comp, usr);
+  finalCount(comp);
+  checkWinner(usr, win)  
+  console.log(" ")
+  console.log(` Your have ${bulls} bulls and ${cows} cows`)
+};
+
+function gameOver() {
+  console.log(` The Game is over.`);
+  console.log(` Computer's number was ${winNumbers.toString().replace(/,/g, "")}\n`);
+  console.log(` YOU LOST`)
+  playAgain();
+};
+
+function checkAttempts() {
+  if(winner == true) {
+    return false;
+  }
+  
+  else if(attempts == 15) {
+    console.log(" ")
+    console.log(` Last chance...Choose wisely`)
+    return true;
+  }
+
+  else if(attempts > 15){
+    gameOver()
+    return false;
+  }
+  else {
+    console.log(` Attempt #${attempts}`)
+    return true;
+  }
+};
+
+function checkPreviousGuess(prevGuess, currGuess) {
+  if (prevGuess.includes(currGuess)){
+    console.log(" ")
+    console.log(` You have already used that number.\n Please enter a different one. \n\n`)
+    attempts --;
+    userGuess();
+  };
+};
+
+
+function userGuess() {
+  let currUserGuess = 0;
+  let arrComp = winNumbers.slice();
+  userInput = readlineSync.question(` What is your number? \n\n`);
+  currUserGuess = userInput.replace(/ /g, '');
+  checkPreviousGuess(arrsUser, currUserGuess);
+  if(Number(currUserGuess) == "NaN" || currUserGuess.length !== 4){
+    console.log(` Please enter a four digit number!\n`);
+    attempts --;
+    userGuess();
+  }
+  else {
+
+    usrGuess = currUserGuess.split("");
+    guesses = currUserGuess.split("").slice()
+    arrsUser.push(usrGuess);
+    console.log(`currUsG: ` + currUserGuess);
+    console.log(`usrGuess: ` + usrGuess);
+    console.log(`ArrUser: ` + arrsUser);
+    console.log(`ArrComp: ` + arrComp);
+    console.log(`winNumbers: ` + winNumbers);
+    
+  }
+  attempts ++;
+  findBullsAndCows(arrComp, usrGuess, winNumbers);
+  console.log(`currUsG: ` + currUserGuess);
+  console.log(`usrGuess: ` + usrGuess);
+  console.log(`ArrUser: ` + arrsUser);
+  console.log(`ArrComp: ` + arrComp);
+  console.log(`winNumbers: ` + winNumbers);
+  console.log(`guesses: ` + guesses);
+  
 }
 
-function finalCount(arr) {
-  for (i = 0; i < arr.length; i++) {
-    if (arr[i] == `/`) {
-      bulls++;
-    }
-    else if (arr[i] == `*`) {
-      cows++;
-    }
-    else {
-      continue;
-    }
+function setUp() {
+  wynNumbers = [];
+  generateNums();
+  arrComp = winNumbers.slice();
+  return arrComp;
+}
+
+function play(arrC) {
+  console.log(arrC);
+  console.log(" ")
+  console.log(` Game on!!`)
+  console.log(" ");
+  console.log(` The computer's number is: XXXX`)
+  do {
+    userGuess();
+  }
+  while( checkAttempts() );
+}
+
+function exit() {
+  console.log(" ")
+  console.log(` Thank you for playing Bulls and Cows!\n\n`);
+  process.exit;
+};
+
+function playNow(){
+  console.log(" ")
+  let playNow = readlineSync.question(` Play game now? (y/n)\n\n`)
+  playNow = playNow.toLowerCase().trim();
+  if (playNow == 'y') {
+    play(arrComp);
+  }
+  else if (playNow == 'n') {
+    console.log(" ")
+    startGame();
+  }
+  else {
+    console.log(" ")
+    console.log(` Please enter 'y' or 'n'\n\n`)
   }
 }
 
+function seeInstructions() {
+  console.log(` Bulls and Cows is a simple guessing game. Your goal is to decipher\n a four digit number that the computer has generated for you.\n In order to do so, you must tell the computer a four digit number\n of your own. If your guess has the right digit at the exact position,\n you will have found a BULL. If your guess has the right digit,\n but in a different position than in the computer's number, you will\n have found a COW. The rest of the digits don't count. \n You have 15 attemps to find the four Bulls. `);
+  playNow();
+}
 
-findBulls(arrComp, arrUser)
-findCows(arrComp, arrUser)
-finalCount(arrComp);
-console.log(" ")
-console.log(`bulls: ` + bulls)
-console.log(`cows: ` + cows)
-// let userNum = readlineSync.question(` Enter your number...`)
 
+function startGame() {
+  console.log(`\n WELCOME TO BULLS AND COWS`);
+  console.log(" ");
+  setUp();
+  let optionNumb = readlineSync.question(` What do you want to do?\n (Insert number)\n\n  - 1: Play\n  - 2: See instructions\n  - 3: Exit \n\n`);
+  optionNumb = Number(optionNumb.trim());
+  if (optionNumb == '1'){
+    console.log(" ")
+    play(arrComp);
+  }
+  else if (optionNumb == '2') {
+    console.log(" ")
+    seeInstructions();
+  } 
+  else if (optionNumb == '3') {
+    console.log(" ")
+    exit()
+  }
+  else {
+    console.log(" ")
+    console.log(` Please enter a number for the option...`)
+    startGame();
+  }
+} 
+
+startGame();
 
 
--- test.js
+++ test.js
@@ -1,67 +1,106 @@
-let bulls = 0;
-let cows = 0;
-let arrComp = [9, 3, 7, 4];
-let arrUser = [4, 3, 4, 9];
+// let bulls = 0;
+// let cows = 0;
+// let arrComp = [9, 3, 7, 4];
+// let arrUser = [4, 3, 4, 9];
 
 
-function findBulls(arr1, arr2) {
-  bulls = 0
-  for (i = 0; i < 4; i++) {
-    if (arr1[i] == arr2[i]) {
-      // bulls += 1;
-      arr1.splice(i, 1, `/`);
-      arr2.splice(i, 1, `/`);
-      console.log(arr1, arr2)
-    };
-  };
-  // return bulls;
-};
+// function findBulls(arr1, arr2) {
+//   bulls = 0
+//   for (i = 0; i < 4; i++) {
+//     if (arr1[i] == arr2[i]) {
+//       // bulls += 1;
+//       arr1.splice(i, 1, `/`);
+//       arr2.splice(i, 1, `/`);
+//       console.log(arr1, arr2)
+//     };
+//   };
+//   // return bulls;
+// };
 
 
-// findBulls(first, second)
-console.log(`arrComp ` + arrComp )
-console.log(`arrUser` + arrUser)
-// console.log(`first` + first)
-// console.log(`second` + second)
+// // findBulls(first, second)
+// console.log(`arrComp ` + arrComp )
+// console.log(`arrUser` + arrUser)
+// // console.log(`first` + first)
+// // console.log(`second` + second)
 
 
-function findCows(arr1, arr2) {
-  cows  = 0
-  for (i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== `/` && arr1[i] !== `*`){
-        for (j = 0; j <arr2.length; j++) {
-          if (arr1[i] == arr2[j]) {
-            console.log(`cows count inside` + cows);
-            arr1.splice(i, 1, `*`);
-            arr2.splice(j, 1, `*`);
-            // cows ++;
-          }
-        }
-        // return cowsCount;
-      }
-    console.log(`cows count final` + cows);
-    console.log(arr1, arr2);  
-    // cows += cowsCount.length;
-  }
-  // return cows
-}
+// function findCows(arr1, arr2) {
+//   cows  = 0
+//   for (i = 0; i < arr1.length; i++) {
+//       if (arr1[i] !== `/` && arr1[i] !== `*`){
+//         for (j = 0; j <arr2.length; j++) {
+//           if (arr1[i] == arr2[j]) {
+//             console.log(`cows count inside` + cows);
+//             arr1.splice(i, 1, `*`);
+//             arr2.splice(j, 1, `*`);
+//             // cows ++;
+//           }
+//         }
+//         // return cowsCount;
+//       }
+//     console.log(`cows count final` + cows);
+//     console.log(arr1, arr2);  
+//     // cows += cowsCount.length;
+//   }
+//   // return cows
+// }
 
-function finalCount(arr) {
-  for (i = 0; i < arr.length; i++){
-    if(arr[i] == `/`) {
-      bulls ++;
-    }
-    else if(arr[i] == `*`){
-      cows ++;
-    }
-    else{
-      continue;
-    }
-  }
-}
+// function finalCount(arr) {
+//   for (i = 0; i < arr.length; i++){
+//     if(arr[i] == `/`) {
+//       bulls ++;
+//     }
+//     else if(arr[i] == `*`){
+//       cows ++;
+//     }
+//     else{
+//       continue;
+//     }
+//   }
+// }
 
-findBulls(arrComp, arrUser);
-findCows(arrComp, arrUser);
-finalCount(arrComp);
-console.log(`bulls ` + bulls);
-console.log(`cows ` + cows);
+// findBulls(arrComp, arrUser);
+// findCows(arrComp, arrUser);
+// finalCount(arrComp);
+// console.log(`bulls ` + bulls);
+// console.log(`cows ` + cows);
+
+// let numbers = []
+
+// function generateNums() {
+//   for (i = 0; i < 4; i++) {
+//     randomNum = Math.random() * 10;
+//     numbers.push(Math.floor(randomNum));
+//     console.log(randomNum)
+//   };
+// };
+
+// notWorking = Number("cant be coerced");
+makeArr2 = {};
+working = Number(" 123 45 5 ");
+working2 = [1,2,3,4]
+working3 = [6,7,8,9]
+
+
+// console.log(notWorking);
+// console.log(working);
+
+// // let makeArr = notWorking.replace(/ /g, '').split('')
+// // console.log(notWorking);
+// // console.log(makeArr);
+// console.log(working);
+
+// let makeArr2;
+// let makeArr1;
+// makeArr2 = working.toString().replace(/ /g, '')
+// makeArr1 = notWorking.toString().replace(/ /g, '')
+makeArr2.push(working2);
+makeArr2.push(working3);
+
+
+console.log(`mk2:` + makeArr2);
+// console.log(`mk1: ` + makeArr1);
+
+// console.log(`mk2:` + makeArr2);
+
